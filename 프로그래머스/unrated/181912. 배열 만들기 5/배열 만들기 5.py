02e3c89c8c2c1b99e0e0bfd6d06e7eeb766a8192
Seungeun Han <101082685+Seungeun-Han@users.noreply.github.com>
2023-11-02 10:54:24 +0900

    [level unrated] Title: 배열 만들기 5, Time: 1.88 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.py
@@ -0,0 +1,8 @@
+def solution(intStrs, k, s, l):
+    answer = []
+    for i in intStrs:
+        check = int(i[s:s+l])
+        if check > k:
+            answer.append(check)
+    return answer
+    # return [int(intstr[s:s+l]) for intstr in intStrs if int(intstr[s:s+l]) > k]
