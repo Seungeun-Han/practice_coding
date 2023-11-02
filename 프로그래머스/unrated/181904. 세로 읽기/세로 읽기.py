22f669ec6906c2e434996e5203ae27942502f797
Seungeun-Han <101082685+Seungeun-Han@users.noreply.github.com>
2023-11-02 15:27:15 +0900

    [level unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -3,4 +3,5 @@ def solution(my_string, m, c):
     # for i in range(len(my_string)//m):
     #     answer += my_string[i*m + (c-1)]
     # return answer
-    return ''.join([my_string[i*m + (c-1)] for i in range(len(my_string)//m)])
+    # return ''.join([my_string[i*m + (c-1)] for i in range(len(my_string)//m)])
+    return my_string[c-1::m]
