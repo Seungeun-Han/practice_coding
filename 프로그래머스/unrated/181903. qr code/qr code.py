9870869ff9fd4556e2e217c844f1cec1bba96ebb
Seungeun-Han <101082685+Seungeun-Han@users.noreply.github.com>
2023-11-02 15:43:37 +0900

    [level unrated] Title: qr code, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.py
+++ 프로그래머스/unrated/181903. qr code/qr code.py
@@ -5,4 +5,5 @@ def solution(q, r, code):
     #     answer += code[i*q+r]
     #     i += 1
     # return answer
-    return code[r::q]
+    # return code[r::q]
+    return ''.join(code[i] for i in range(r, len(code), q))
