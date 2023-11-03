57a1f264be47c58c07142b917a8eee4d9ae101fb
Seungeun-Han <101082685+Seungeun-Han@users.noreply.github.com>
2023-11-03 18:03:08 +0900

    [level unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -1,8 +1,9 @@
 def solution(myString, pat):
-    answer = 0
+#     answer = 0
 
-    for i in range(len(myString)):
-        if pat == myString[i:i+len(pat)]:
-            answer += 1
+#     for i in range(len(myString)):
+#         if pat == myString[i:i+len(pat)]:
+#             answer += 1
 
-    return answer
+#     return answer
+    return sum(myString[i:i + len(pat)] == pat for i in range(len(myString)))
