61f4c2274fac7be17e3d1789194aabe740ea4e87
Seungeun-Han <101082685+Seungeun-Han@users.noreply.github.com>
2023-11-03 14:54:44 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,7 +1,14 @@
-def solution(str_list):
-    for i, v in enumerate(str_list):
-        if v in ['l', 'r']:
-            idx, value = i, v
-            break
+# def solution(str_list):
+#     for i, v in enumerate(str_list):
+#         if v in ['l', 'r']:
+#             idx, value = i, v
+#             break
             
-    return str_list[:i] if v=='l' else str_list[i+1:]
+#     return str_list[:i] if v=='l' else str_list[i+1:]
+
+
+def solution(str_list):
+    for i in range(len(str_list)):
+        if str_list[i]=='l': return str_list[:i]
+        elif str_list[i]=='r': return str_list[i+1:]
+    return []
