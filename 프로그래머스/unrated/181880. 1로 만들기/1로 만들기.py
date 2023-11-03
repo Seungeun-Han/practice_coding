2e589a8c8e00bc8d3d0609a36d106303c44ae9c7
Seungeun-Han <101082685+Seungeun-Han@users.noreply.github.com>
2023-11-03 16:21:18 +0900

    [level unrated] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
@@ -1,12 +1,16 @@
-def solution(num_list):
-    answer = 0
+# def solution(num_list):
+#     answer = 0
     
-    for n in num_list:
-        if n == 1: 
-            continue 
-        for i in range(1, 5):
-            if n >= 2**i and n < 2**(i+1):
-                answer += i
-                break
+#     for n in num_list:
+#         if n == 1: 
+#             continue 
+#         for i in range(1, 5):
+#             if n >= 2**i and n < 2**(i+1):
+#                 answer += i
+#                 break
         
-    return answer
+#     return answer
+
+
+def solution(num_list):
+    return sum(len(bin(i)) - 3 for i in num_list)
