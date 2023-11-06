e57845624f38df7a233d998c7a419ec8ae0d9ace
Seungeun-Han <101082685+Seungeun-Han@users.noreply.github.com>
2023-11-06 10:55:23 +0900

    [level unrated] Title: 문자열 묶기, Time: 22.01 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
@@ -2,8 +2,9 @@ def solution(strArr):
     count_dict = {}
     
     for l in strArr:
-        if len(l) not in count_dict.keys():
-            count_dict[len(l)] = 0
-        count_dict[len(l)] += 1
+        # if len(l) not in count_dict.keys():
+        #     count_dict[len(l)] = 0
+        # count_dict[len(l)] += 1
+        count_dict[len(l)] = count_dict.get(len(l), 0) + 1
     
     return max(count_dict.values())
